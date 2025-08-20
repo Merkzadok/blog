42752dc0b14594fb4a80532d269b29028403d90b
merkzadok <merkzadok@Gmail.com>
2025-08-20 18:10:21 +0800

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media2.dev.to",
+      },
+      {
+        protocol: "https",
+        hostname: "dev-to-uploads.s3.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media.dev.to",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
